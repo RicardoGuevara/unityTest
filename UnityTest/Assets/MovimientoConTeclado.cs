71f4a91bd23eb9a3db3f321faeff5dd39f1f3c12
Juan Bojato <juandavidcancer@hotmail.com>
2018-11-01 22:05:04 -0500

    sdk, colisiones

---
+++ UnityTest/Assets/MovimientoConTeclado.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovimientoConTeclado : MonoBehaviour
+{
+    public int m_velocidad;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+
+            transform.Translate(Time.deltaTime * m_velocidad, 0, 0);
+
+        if (Input.GetKey(KeyCode.A))
+
+            transform.Translate(Time.deltaTime * 0, 0, 0.1f);
+
+        if (Input.GetKey(KeyCode.S))
+
+            transform.Translate(-Time.deltaTime * m_velocidad, 0, 0);
+
+        if (Input.GetKey(KeyCode.D))
+
+            transform.Translate(Time.deltaTime, 0, -0.1f);
+
+    }
+}
+
