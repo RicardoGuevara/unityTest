da209ac6f90fa8923183e7fda2617331f4df856e
Juan Bojato <juandavidcancer@hotmail.com>
2018-11-09 02:35:38 -0500

    Menu

---
+++ UnityTest/Assets/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+    public GameObject a;
+    public GameObject Capsula;
+
+    public void Boton() {
+        a.SetActive(false);
+
+        Vector3 temp = new Vector3(60f, 2, 0f);
+        Capsula.transform.position += temp;
+    }
+    public void Boton2() {
+        a.SetActive(false);
+        Vector3 temp = new Vector3(20f, 1.65f, 118.56f);
+        Capsula.transform.position += temp;
+    }
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
